774677acfab68a5e6d5ff1779a98725705c76744
geckocoding <hello.derpdex@gmail.com>
2023-06-06 16:09:41 +0800

    Add WETH addresses

--- src/entities/weth9.ts
+++ src/entities/weth9.ts
@@ -14,5 +14,8 @@ export const WETH9: { [chainId: number]: Token } = {
   [69]: new Token(69, '0x4200000000000000000000000000000000000006', 18, 'WETH', 'Wrapped Ether'),
 
   [42161]: new Token(42161, '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', 18, 'WETH', 'Wrapped Ether'),
-  [421611]: new Token(421611, '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681', 18, 'WETH', 'Wrapped Ether')
+  [421611]: new Token(421611, '0xB47e6A5f8b33b3F17603C83a0535A9dcD7E32681', 18, 'WETH', 'Wrapped Ether'),
+
+  [280]: new Token(280, '0xC3ec043C150c945652A09D7E47F856AC9fB0F893', 18, 'WETH', 'Wrapped Ether'),
+  [324]: new Token(324, '0x5aea5775959fbc2557cc8789bc1bf90a239d9a91', 18, 'WETH', 'Wrapped Ether'),
 }
